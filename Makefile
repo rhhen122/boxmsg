fbca2059d50f2fcc8867f33b66a0058c78e20e02
rhhen122 <rokyhenderson222@gmail.com>
2025-07-12 14:04:31 +1200

    Added MINI

--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
 CC=gcc
 all: boxmsg.c
+	rm -f box
+	$(CC) boxmsg.c -o box
+mini: boxmsg-mini.c
+	rm -f box
 	$(CC) boxmsg.c -o box
 clean:
 	rm -f box
---
+++ boxmsg-mini.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    int length = strlen(argv[1]);
+    char hy[1001];
+    int i = 0;
+    while (1) {
+        strcat(hy, "-");
+        i += 1;
+        if (i == length) {
+            break;
+        }
+        printf("+--%s--+\n|  %s  |\n+--%s--+\n", hy, argv[1], hy);
+    }
+    return EXIT_SUCCESS;
+}
